9d28e390ea06186e8fd2dce62e4a6ab27e9204a1
Geoffrey Andrade <geoffandrade@hotmail.com>
2020-09-14 21:07:28 -0400

    update page load- get metadat functions

--- solution/solution.js
+++ solution/solution.js
@@ -1,70 +1,86 @@
-//Grab article attributes to pass in Pageview tracking:
-function getTitle () {
-  const titleClass = document.getElementsByTagName("h2");
-  console.log(`titleclass: ${titleClass}`)
-  const title = titleClass.innerText;
-  console.log(`property returned: ${title}`)
-  return title;
-};
+window.addEventListener('load', function () {
 
-  // const titleClass = document.getElementsByClassName("content");
-  // const title = titleClass.querySelector("h2").textContent
 
-function getAuthor() {
-  const authorElem = document.getElementsByTagName("b");
-  console.log(`author element: ${authorElem}`)
-  const authorText = authorElem.innerText;
-  console.log(`property returned: ${authorText}`)
-  return authorText;
-};
+  //Grab article attributes to pass in Pageview tracking:
+  function getTitle () {
+    const titleClass = document.getElementsByTagName("h2")[0];
+    console.log(`titleclass: ${titleClass}`)
+    let title = titleClass.innerText;
+    console.log(`Title returned: ${title}`)
+    return title;
+  };
 
-// const options = {
-//   success: function(event) {
-//     console.log("Tracked a new " + event.name + " event.");
-//     console.log(event)
-//   },
-//   error: function(errors) {
-//   }
-// }
-//
-// // Track an "EmailSubscription" event, with the property
-// // `newletter` set as `true`.
-// permutive.track('Pageview', {
-//   title: getTitle(),
-//   author: getAuthor(),
-//   categories: ["string1", "string2", "string3"]
-// },options);
+  function getAuthor() {
+    const authorElem = document.getElementsByTagName("b")[0];
+    console.log(`author element: ${authorElem}`)
+    const authorText = authorElem.innerText;
+    console.log(`Author returned: ${authorText}`)
+    return authorText;
+  };
 
-/* API Fetch:
-fetch ('https://api.permutive.com/v2.0/events', {
-    method: 'POST',
-    headers: {
-        'X-API-Key': '0a1c1596-66d2-4939-9955-3c04dab4b0e2',
-        'Content-Type': 'application/json'
+  const options = {
+    success: function(event) {
+      console.log(`success callback ${JSON.stringify(event)}`)
+      //console.log("Tracked a new " + event.name + " event.");
+      //console.log(`in options ${JSON.stringify(event)}`)
     },
-    body: JSON.stringify({
-      "user_id": "8cc26071-25a7-4033-b2c6-ff0fcb5e04a3",
-      "name": "EmailSubscription",
-      "properties": {
-        "email": "mark@facebook.com",
-        "subscriptions": {
-          "newsletter": true,
-          "updates": false
-        }
-      }
-    })
-});
-*/
-
-permutive.segments(function(segments) {
-  for (var idx in segments) {
-    // The user is currently in this segment
-    console.log("User is currently in Permutive segment: " + segments[idx]);
+    error: function(errors) {
+      console.log(`failure callback ${JSON.stringify(errors)}`)
+    }
   }
-});
-//GET THE PERMUTIVE ID:
-console.log(localStorage.getItem("permutive-id"))
 
+  // // Track an "EmailSubscription" event, with the property
+  // // `newletter` set as `true`.
+  permutive.track('Pageview', {
+    title: getTitle(),
+    author: getAuthor(),
+    categories: ["string1", "string2", "string3"]
+  }, options);
 
-//google publisher tag
-//window.googletag.pubads()
+  /* API Fetch:
+  fetch ('https://api.permutive.com/v2.0/events', {
+      method: 'POST',
+      headers: {
+          'X-API-Key': '0a1c1596-66d2-4939-9955-3c04dab4b0e2',
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "user_id": "8cc26071-25a7-4033-b2c6-ff0fcb5e04a3",
+        "name": "EmailSubscription",
+        "properties": {
+          "email": "mark@facebook.com",
+          "subscriptions": {
+            "newsletter": true,
+            "updates": false
+          }
+        }
+      })
+  });
+  */
+
+  function segTest () {
+    permutive.segment(6912, function(result) {
+      if (result) {
+        console.log(`user is in ${result}`)
+      } else {
+        console.log('else reached')
+      }
+    });
+  }
+
+  //GET THE PERMUTIVE ID:
+  //console.log(localStorage.getItem("permutive-id"))
+
+  //google publisher tag
+  //window.googletag.pubads()
+  window.googletag = window.googletag || {};
+  window.googletag.cmd = window.googletag.cmd || [];
+  window.googletag.cmd.push(function () {
+    // set targeting here
+        // Page-level targeting.
+        window.googletag.pubads().setTargeting('permutive', 'gaming');
+
+        googletag.enableServices();
+  });
+
+}) // End of window.load
